99b3df1181e049d91fc16801c95dbce7ef9818b5
Nikita Solovyev <mr.utingen@gmail.com>
2024-12-05 13:42:58 +0300

    Initial commit

---
+++ TravelSchedule/TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Гена Утин on 05.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
